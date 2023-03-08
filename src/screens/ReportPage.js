a99730432ecc6382fa475c673ae1116afacc11d2
jasmine822 <jasmineikhy@gmail.com>
2023-03-08 09:13:56 -0800

    Update

--- src/screens/ReportPage.js
+++ src/screens/ReportPage.js
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {styles} from '../components/styles';
 import TopView from '../components/TopView';
@@ -6,7 +6,21 @@ import TopView from '../components/TopView';
 const ReportPage = () => {
   return (
     <View style={styles.container}>
-      <TopView text={"Report Page"}/>
+      <TopView text={'Report Page'} />
+
+      <View style={styles.reportPageContainer}>
+        <View style={styles.reportPageRow}>
+          <TouchableOpacity style={styles.reportPageButton}>
+            <Text>Button 1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.reportPageButton}>
+            <Text>Button 2</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.reportPageButton}>
+          <Text>Button 3</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
